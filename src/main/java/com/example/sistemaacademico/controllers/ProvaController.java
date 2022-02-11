6c62b75ae327192c9bfeb06504f4b0926bda95c7
Yan Sérgio Yudi Ajiki <yan.ajiki@MJV.net>
2022-02-11 09:16:41 -0400

    [wip] endpoints CRUD Prova

--- src/main/java/com/example/sistemaacademico/controllers/ProvaController.java
+++ src/main/java/com/example/sistemaacademico/controllers/ProvaController.java
@@ -1,2 +1,26 @@
-package com.example.sistemaacademico.controllers;public class ProvaController {
+package com.example.sistemaacademico.controllers;
+
+import com.example.sistemaacademico.requestBody.LancarNotaBody;
+import com.example.sistemaacademico.services.LancamentoNotaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/prova")
+public class ProvaController {
+
+
+    private final LancamentoNotaService lancamentoNotaService;
+
+    @Autowired
+    public ProvaController(LancamentoNotaService lancamentoNotaService) {
+        this.lancamentoNotaService = lancamentoNotaService;
+    }
+
+
+    @PostMapping("/lancarNota")
+    public void lancarNota(@RequestBody LancarNotaBody reqBody){
+        lancamentoNotaService.lancarNotaNaProva(reqBody.getProvaId(), reqBody.getNota());
+    }
 }
